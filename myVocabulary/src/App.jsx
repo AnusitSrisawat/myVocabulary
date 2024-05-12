0c7ae4f24cd50f33ef362fc4ebd8eb30748a7923
Jimmy <jimmy18778@gmail.com>
2024-05-13 01:41:46 +0700

    game page

--- myVocabulary/src/App.jsx
+++ myVocabulary/src/App.jsx
@@ -30,16 +30,22 @@ function App() {
 
   return (
     <>
-      <div className='bg-slate-700 shadow-xl bg-opacity-30 p-10 rounded-3xl flex flex-col justify-center items-center gap-6'>
-        <div className='flex flex-col justify-start items-center gap-4'>
-          <div className='rounded-full bg-red-600 w-8 h-8'></div>
-          <div className='break-words whitespace-normal'>มดตัวน้อยตัวนิด</div>
+      <div className='relative bg-slate-700 shadow-xl bg-opacity-30 rounded-3xl flex flex-col justify-center items-center'>
+        <div className='p-10 rounded-3xl flex flex-col justify-center items-center gap-6'>
+          <div className='flex flex-col justify-start items-center gap-4'>
+            <div className='rounded-full bg-red-600 w-8 h-8'></div>
+            <div className='break-words whitespace-normal'>มดตัวน้อยตัวนิด</div>
+          </div>
+          <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center'>
+            <div className='w-full bg-slate-600 bg-opacity-10 text-left px-4 py-3 rounded-xl cursor-pointer border-2 border-slate-600 hover:border-slate-500 hover:bg-slate-500 hover:shadow-lg hover:scale-105 active:scale-90 duration-200'>A. <span>ANT!!!!</span> </div>
+            <div className='w-full bg-slate-600 bg-opacity-10 text-left px-4 py-3 rounded-xl cursor-pointer border-2 border-slate-600 hover:border-slate-500 hover:bg-slate-500 hover:shadow-lg hover:scale-105 active:scale-90 duration-200'>B. <span>ant little</span> </div>
+            <div className='w-full bg-slate-600 bg-opacity-10 text-left px-4 py-3 rounded-xl cursor-pointer border-2 border-slate-600 hover:border-slate-500 hover:bg-slate-500 hover:shadow-lg hover:scale-105 active:scale-90 duration-200'>C. <span>ant tiny</span> </div>
+            <div className='w-full bg-slate-600 bg-opacity-10 text-left px-4 py-3 rounded-xl cursor-pointer border-2 border-slate-600 hover:border-slate-500 hover:bg-slate-500 hover:shadow-lg hover:scale-105 active:scale-90 duration-200'>D. <span>ant</span> </div>
+          </div>
         </div>
-        <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-3 justify-center items-center'>
-          <div className='w-full bg-slate-600 bg-opacity-10 text-left px-4 py-3 rounded-xl cursor-pointer border-2 border-slate-600 hover:border-slate-500 hover:bg-slate-500 hover:shadow-lg active:scale-90 duration-200'>A. <span>ANT!!!!</span> </div>
-          <div className='w-full bg-slate-600 bg-opacity-10 text-left px-4 py-3 rounded-xl cursor-pointer border-2 border-slate-600 hover:border-slate-500 hover:bg-slate-500 hover:shadow-lg active:scale-90 duration-200'>B. <span>ant little</span> </div>
-          <div className='w-full bg-slate-600 bg-opacity-10 text-left px-4 py-3 rounded-xl cursor-pointer border-2 border-slate-600 hover:border-slate-500 hover:bg-slate-500 hover:shadow-lg active:scale-90 duration-200'>C. <span>ant tiny</span> </div>
-          <div className='w-full bg-slate-600 bg-opacity-10 text-left px-4 py-3 rounded-xl cursor-pointer border-2 border-slate-600 hover:border-slate-500 hover:bg-slate-500 hover:shadow-lg active:scale-90 duration-200'>D. <span>ant</span> </div>
+        <div className='w-full h-full top-0 left-0 flex justify-between items-center p-3'>
+          <div className='cursor-pointer hover:scale-105 active:scale-90 duration-100 text-md flex justify-center items-center text-center bg-slate-600 px-2 py-1 rounded-xl'>previus</div>
+          <div className='cursor-pointer hover:scale-105 active:scale-90 duration-100 text-md flex justify-center items-center text-center bg-slate-600 px-2 py-1 rounded-xl'>next</div>
         </div>
       </div>
     </>

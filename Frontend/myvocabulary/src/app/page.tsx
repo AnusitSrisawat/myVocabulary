aaa53814511287a67fe0fedd52aeff90fc4aa3fc
Jimmy <jimmy18778@gmail.com>
2024-05-14 23:42:48 +0700

    .

--- Frontend/myvocabulary/src/app/page.tsx
+++ Frontend/myvocabulary/src/app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
         <Link href="/Game">
           <div className='bg-slate-500 bg-opacity-30 p-5 rounded-2xl flex flex-row justify-center items-center gap-2 min-w-48 border-2 border-slate-600 hover:border-slate-500 hover:bg-slate-500 hover:shadow-lg hover:scale-105 active:scale-90 duration-200 cursor-pointer'>
             <div className='rounded-full w-10 h-10'>
-              {/* <img src="/game.svg" alt="game" className='w-full h-full hover:scale-105 active:scale-90 cursor-pointer duration-200 /> */}
               <img src="/gameWhite.svg" alt="game" className='w-full h-full hover:scale-105 active:scale-90 cursor-pointer duration-200' />
             </div>
             <div className='break-words whitespace-normal text-xl'>Game</div>
@@ -23,22 +22,20 @@ export default function Home() {
         <Link href="/Words">
           <div className='bg-slate-500 bg-opacity-30 p-5 rounded-2xl flex flex-row justify-center items-center gap-2 min-w-48 border-2 border-slate-600 hover:border-slate-500 hover:bg-slate-500 hover:shadow-lg hover:scale-105 active:scale-90 duration-200 cursor-pointer'>
             <div className='rounded-full w-10 h-10'>
-              {/* <img src="/words.svg" alt="words" className='w-full h-full hover:scale-105 active:scale-90 cursor-pointer duration-200 /> */}
               <img src="/wordsWhite.svg" alt="words" className='w-full h-full hover:scale-105 active:scale-90 cursor-pointer duration-200' />
             </div>
             <div className='break-words whitespace-normal'>Words</div>
           </div>
         </Link>
 
-        <Link href="/Setting">
+        {/* <Link href="/Setting">
           <div className='bg-slate-500 bg-opacity-30 p-5 rounded-2xl flex flex-row justify-center items-center gap-2 min-w-48 border-2 border-slate-600 hover:border-slate-500 hover:bg-slate-500 hover:shadow-lg hover:scale-105 active:scale-90 duration-200 cursor-pointer'>
             <div className='rounded-full w-10 h-10'>
-              {/* <img src="/setting.svg" alt="setting" className='w-full h-full hover:scale-105 active:scale-90 cursor-pointer duration-200 /> */}
               <img src="/settingWhite.svg" alt="setting" className='w-full h-full hover:scale-105 active:scale-90 cursor-pointer duration-200' />
             </div>
             <div className='break-words whitespace-normal'>Setting</div>
           </div>
-        </Link>
+        </Link> */}
 
       </div>
     </main>
